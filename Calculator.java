d896351269dd28c2304bcbe2b3b7720460badde8
Jude Policarpio <100894687+Judezzzy@users.noreply.github.com>
2024-09-04 22:14:37 +0800

    Created Scanner for Calculator

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class CalculatorWithScanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+
+        System.out.println("Enter the first number:");
+        int a = scanner.nextInt(); 
+
+        System.out.println("Enter the second number:");
+        int b = scanner.nextInt(); 
